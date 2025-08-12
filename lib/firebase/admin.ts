2fe7f94f45f7f267af8c409d3c616e78a6a2aa0e
Emoji <earth2luke@users.noreply.github.com>
2025-08-11 23:10:09 -0700

    Create admin.ts

---
+++ lib/firebase/admin.ts
@@ -0,0 +1,19 @@
+import { cert, getApps, initializeApp, AppOptions } from "firebase-admin/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+import getStorage } from "firebase-admin/storage";
+
+const options: AppOptions = {
+  credential: cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  }),
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+};
+
+const adminApp = getApps().length ? getApps()[0]! : initializeApp(options);
+
+export const adminAuth = getAuth(adminApp);
+export const adminDb = getFirestore(adminApp);
+export const adminStorage = getStorage(adminApp);
